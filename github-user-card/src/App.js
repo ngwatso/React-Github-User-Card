9a0dfd580316158ed6104255dd2bed954f873e0c
ngwatso <ngwatson79@yahoo.com>
2021-01-15 21:19:09 -0500

    added UserCard, UserForm components, set up state

--- github-user-card/src/App.js
+++ github-user-card/src/App.js
@@ -1,7 +1,39 @@
+import React from "react";
 import "./App.css";
+import axios from "axios";
 
-function App() {
-	return <div className="App"></div>;
+class App extends React.Component {
+	//  TODO Set up constructor to store state
+
+	constructor() {
+		super();
+		this.state = { userInfo: [], user: "", newUser: "" };
+	}
+
+	// TODO Mount component and do axios pull
+	componentDidMount() {
+		axios.get("https://api.github.com/users/ngwatso")
+			.then((res) => {
+				console.log(
+					`nw: App.js: componentDidMount: axiosGet: res:`,
+					res.data
+				);
+			})
+			.catch((err) => console.error(`ERROR RETRIEVING DATA`, err));
+	}
+
+	render() {
+		return (
+			<>
+				<h1>Github</h1>
+				<input
+					value={this.state.newUser}
+					onChange={this.handleChange}
+				/>
+			</>
+		);
+	}
+	// return <div className="App"></div>;
 }
 
 export default App;
---
+++ github-user-card/src/components/UserForm.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import "./App.css";
+
+// TODO Create form for entering username
+
+class UserForm extends React.Component {
+	// TODO Create construstor to store state
+	constructor() {
+		super();
+		this.state = {
+			username: "",
+		};
+	}
+}
+
+export default UserForm;
